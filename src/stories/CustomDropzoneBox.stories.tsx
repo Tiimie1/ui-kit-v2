7b1047a7eb034202d692fe1f9250dbc3a9d51050
Tiimie1 <tim.pangersic@gmail.com>
2024-06-26 15:52:00 +0200

    fix(ui-kit): added necesarry packages, small fixes

--- src/stories/CustomDropzoneBox.stories.tsx
+++ src/stories/CustomDropzoneBox.stories.tsx
@@ -1,27 +1,32 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import CustomDropzoneBox from "@/components/CustomDropzoneBox";
+import { Wrapper } from "./Wrapper";
 
 const meta: Meta<typeof CustomDropzoneBox> = {
-  title: "Forms/CustomDropzoneBox",
+  title: "CustomDropzoneBox",
   component: CustomDropzoneBox,
   parameters: {
     layout: "centered",
   },
-  tags: ["autodocs"],
 };
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const CustomDropzoneBoxStory: Story = {
+  render: (args) => (
+    <Wrapper>
+      <CustomDropzoneBox {...args} />
+    </Wrapper>
+  ),
   args: {
     uploadText: "Upload a file",
     dragText: "or drag and drop",
     fileTypesText: "PDF, DOC, JPG up to ",
-    fileSizeLimitText: '10MB',
+    fileSizeLimitText: "10MB",
     borderColor: "#D1D5DB",
-    textColor: "#6B7280", 
-    iconColor: "#9CA3AF" 
+    textColor: "#6B7280",
+    iconColor: "#9CA3AF",
   },
 };
