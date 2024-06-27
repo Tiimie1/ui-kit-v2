ee84f327654a1a6d542e62e025712d3dce030f80
pzagardedukic <petra.zagar.dedukic@gmail.com>
2024-06-27 13:20:53 +0200

    feat(frontend): Storybook - CustomCreateButton with/without Tooltip.

--- src/stories/CustomCreateButton.stories.tsx
+++ src/stories/CustomCreateButton.stories.tsx
@@ -7,13 +7,23 @@ export default {
   argTypes: {
     onClick: { action: 'clicked' },
     color: { control: 'color' },
+    tooltipText: { control: 'text', description: 'Optional tooltip text' },
   },
 } as Meta<typeof CustomCreateButton>;
 
 const Template: StoryFn<typeof CustomCreateButton> = (args) => <CustomCreateButton {...args} />;
 
-export const CustomCreateButtonStory = Template.bind({});
-CustomCreateButtonStory.args = {
+export const CustomCreateButtonWithoutTooltip = Template.bind({});
+CustomCreateButtonWithoutTooltip.args = {
   label: 'Create SPV',
   color: '#5F66FF',
 };
+
+export const CustomCreateButtonWithTooltip = Template.bind({});
+CustomCreateButtonWithTooltip.args = {
+  label: 'Create SPV',
+  color: '#5F66FF',
+  tooltipText: 'This is a tooltip text',
+};
+
+
