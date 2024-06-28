a504f6f636aa7652ed851192049d7b7e56ee6b74
pzagardedukic <petra.zagar.dedukic@gmail.com>
2024-06-28 10:16:06 +0200

    feat(frontend): Storybook - CustomCard.

--- src/components/CustomCard.tsx
+++ src/components/CustomCard.tsx
@@ -1,65 +1,34 @@
-import { Box, Button, IconButton, Typography } from '@mui/material';
-import { useState } from 'react';
-
-import CommunityIcon from '../../public/Icons/communityIcon';
-import CopyTemplateIcon from '../../public/Icons/copyTemplateIcon';
-import DotsIcon from '../../public/Icons/dotsIcon';
-
-import CustomPopover from './CustomPopover';
-
-import EditIcon from '@mui/icons-material/Edit'
-import PoolIcon from '@mui/icons-material/Pool'
-
-export const popoverItems = [
-  { text: 'Edit template', icon: <EditIcon /> },
-  { text: 'Duplicate template', icon: <PoolIcon /> },
-  { text: 'Delete template', icon: <PoolIcon /> }
-]
+import { Box, Typography } from '@mui/material';
+import { ReactNode } from 'react';
 
 interface CustomCardProps {
   title: string;
   description: string;
-  isPublished?: boolean;
-  maxWidth?: string; 
-  onClick?: () => void;
+  onSelect?: () => void;
+  icon: ReactNode;
+  backgroundColor?: string;
 }
 
 const CustomCard: React.FC<CustomCardProps> = ({
   title,
   description,
-  isPublished,
-  maxWidth,
-  onClick
+  onSelect,
+  icon,
+  backgroundColor = '#ffffff'
 }) => {
-  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
-
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
   return (
     <Box
       sx={{
         borderRadius: '16px',
-        background: '#ffffff',
+        background: backgroundColor,
         padding: 2,
-        cursor: 'pointer',
-        maxWidth: maxWidth, 
-        '&:hover .hover-it': {
-          display: 'block'
-        }
+        cursor: 'pointer'
       }}
+      onClick={onSelect}
     >
       <Box display='flex' flexDirection='column' justifyContent='space-between' minHeight={170}>
         <Box display='flex' justifyContent='space-between' alignItems='center' mb={2}>
-          <CommunityIcon />
-          <IconButton className='hover-it' sx={{ display: 'none' }} onClick={isPublished ? handleClick : () => {}}>
-            {isPublished ? <DotsIcon /> : <CopyTemplateIcon />}
-          </IconButton>
+          {icon}
         </Box>
         <Box>
           <Typography variant='body1' fontWeight={500}>
@@ -69,13 +38,7 @@ const CustomCard: React.FC<CustomCardProps> = ({
             {description}
           </Typography>
         </Box>
-        <Box className='hover-it' sx={{ display: 'none', width: '100%', mt: 2 }}>
-          <Button fullWidth variant='contained' sx={{ borderRadius: '100px' }} onClick={onClick}>
-            {isPublished ? 'Use Template' : 'Preview Template'}
-          </Button>
-        </Box>
       </Box>
-      {isPublished && <CustomPopover items={popoverItems} anchorEl={anchorEl} onClose={handleClose} />}
     </Box>
   );
 };
